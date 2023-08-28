be7ef39a0747981d9701d9f3c6d56c7793c57e7d
Elibe-dave <elibe.dave@gmail.com>
2023-08-27 17:39:35 -0700

    making betty compliant

--- monty.c
+++ monty.c
@@ -2,24 +2,24 @@
 global_var var_global;
 
 /**
- * main - driver function for monty program
- * @ac: int num of arguments
- * @av: opcode file
- * Return: 0
- */
- 
+* main - driver function for monty program
+* @ac: int num of arguments
+* @av: opcode file
+* Return: 0
+*/
+
 int main(int ac, char **av)
 {
-	stack_t *stack;
+stack_t *stack;
 
-	stack = NULL;
-	if (ac != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+stack = NULL;
+if (ac != 2)
+{
+fprintf(stderr, "USAGE: monty file\n");
+exit(EXIT_FAILURE);
+}
 
-	read_file(av[1], &stack);
-	free_dlistint(stack);
-	return (0);
+read_file(av[1], &stack);
+free_dlistint(stack);
+return (0);
 }
